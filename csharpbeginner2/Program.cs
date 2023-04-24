02164e0b3b0530d7e1c2c4e9f74464760e1d1f8d
maxai <maxai@MAX>
2023-04-24 20:32:57 +0800

    Add project files.

---
+++ csharpbeginner2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace csharpbeginner2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var formatedstring = returnDateTime(DateTime.Now, DateTime.Now.ToString("t"));
+            Console.WriteLine(formatedstring);
+            
+            
+        }
+        private static string returnDateTime(DateTime myDatetime, string dateFormat)
+        {
+
+            return string.Format(dateFormat, myDatetime.Date);
+        }
+    }
+}
